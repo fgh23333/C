bfbf760d45b407b78d3cb7acf29935cb35ef7ee4
fgh23333 <98164347+fgh23333@users.noreply.github.com>
2023-09-25 11:13:52 +0800

    change

---
+++ struct.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    struct Student
+    {
+        int num;
+        float scole;
+        char name[20];
+    }
+    stu[50]={
+        {01,12.5,"qq"},
+        {02,90,"ww"},
+        {03,83.6}
+        };
+
+}
